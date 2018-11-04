82ec0af81bb1bc50eed8c125a4fca5eab119bf35
Hannes Hertach <hanneshertach@gmail.com>
2018-11-04 14:00:24 +0000

    Redirect to login page instead of showing 403

--- src/main/java/com/hanneshertach/blog/admin/AdminController.java
+++ src/main/java/com/hanneshertach/blog/admin/AdminController.java
@@ -115,7 +115,7 @@ public class AdminController {
   public static void redirectIfNotLoggedIn(Request request, Response response) {
 
     if (request.session(false) == null) {
-      response.redirect("/", 403);
+      response.redirect("/admin/login");
     }
 
   }
